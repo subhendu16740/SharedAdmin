1ca4d9cce6d346fd101d2d9b5be779bb7e98aa1b
subhendu16740 <31115324+subhendu16740@users.noreply.github.com>
2020-06-14 02:24:02 +0530

    Update Fire.js

--- Fire.js
+++ Fire.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
 import '@firebase/firestore'
 const firebaseconfig={
-    apiKey: "AIzaSyCkjMo5_DP5LOPNYJzM0zPNzXx8H4VLSRo",
-    authDomain: "shared-a7555.firebaseapp.com",
-    databaseURL: "https://shared-a7555.firebaseio.com",
-    projectId: "shared-a7555",
-    storageBucket: "shared-a7555.appspot.com",
-    messagingSenderId: "887377171929",
-    appId: "1:887377171929:web:f12182d38f9310fc0135c8",
-    measurementId: "G-Z4Y12GSTL4"
- 
+    FIREBASEKEYS 
 };
 
 
