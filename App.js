e54ee03f513d713b8e1da886a8efaebc327e105b
subhendu16740 <31115324+subhendu16740@users.noreply.github.com>
2020-06-16 14:56:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -12,14 +12,7 @@ if(!global.btoa){global.btoa=encode}
 if(!global.atob){global.atob=decode}
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCkjMo5_DP5LOPNYJzM0zPNzXx8H4VLSRo",
-    authDomain: "shared-a7555.firebaseapp.com",
-    databaseURL: "https://shared-a7555.firebaseio.com",
-    projectId: "shared-a7555",
-    storageBucket: "shared-a7555.appspot.com",
-    messagingSenderId: "887377171929",
-    appId: "1:887377171929:web:f12182d38f9310fc0135c8",
-    measurementId: "G-Z4Y12GSTL4"
+    Firebase Keys
 })
 
 export default class App extends React.Component{
